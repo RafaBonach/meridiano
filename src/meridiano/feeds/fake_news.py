93a7d26b54f85a93e49ce865ebb750371066b7ab
RafaBonach <rafa.bonach@gmail.com>
2026-02-18 13:58:03 -0300

    Corrigindo o feed fake_news

--- src/meridiano/feeds/fake_news.py
+++ src/meridiano/feeds/fake_news.py
@@ -1,14 +1,48 @@
-RSS_FEEDS = []
+RSS_FEEDS = [
+    "https://feeds.folha.uol.com.br/poder/rss091.xml",  # Folha de S.Paulo - Poder
+    "https://feeds.folha.uol.com.br/ciencia/rss091.xml",  # Folha de S.Paulo - Ciência
+    "https://feeds.folha.uol.com.br/opiniao/rss091.xml",  # Folha de S.Paulo - Opinião
+    "https://feeds.folha.uol.com.br/educacao/rss091.xml",  # Folha de S.Paulo - Educação
+    "https://feeds.folha.uol.com.br/equilibrioesaude/rss091.xml",  # Folha de S.Paulo - Equilíbrio e Saúde
+    "https://www.tribunapr.com.br/feed/",  # Tribuna PR
+    "https://www.bahianoticias.com.br/principal/rss.xml",  # Bahia Notícias
+    "https://www.bahianoticias.com.br/justica/rss.xml",  # Bahia Notícias - Justiça
+    "https://jornaldaparaiba.com.br/feed/",  # Jornal da Paraíba
+    "https://rss.uol.com.br/feed/noticias.xml",  # UOL Notícias
+    "https://pox.globo.com/rss/oglobo", # O Globo
+    "https://pox.globo.com/rss/extra/", # Extra
+    "https://ndmais.com.br/feed/", # ND Mais
+    "https://www.folhavitoria.com.br/feed/", # Folha Vitória
+    "https://diariodorio.com/feed/", # Diário do Rio
+    "https://www.brasildefato.com.br/feed/", # Brasil de Fato
+    "https://www.aliadosbrasiloficial.com.br/rss.xml", # Aliados Brasil
+    "https://www.metropoles.com/feed", # Metrópoles
+    "https://tribunadonorte.com.br/feed/", # Tribuna do Norte
+    "https://jovempan.com.br/jpnews/feed", # Jovem Pan - JPNews
+    "https://www.aosfatos.org/noticias/feed/", # Aos Fatos
+    "https://www.agencialupa.org/feed/", # Agência Lupa
+    "https://www.estadao.com.br/arc/outboundfeeds/feeds/rss/sections/geral/?body=%7B%22layout%22:%22google-news%22%7D", # Estadão - Geral
+    "https://apublica.org/feed/", # Agência Pública
+    "https://www.nexojornal.com.br/rss.xml",  # Nexo Jornal
+    "http://agenciabrasil.ebc.com.br/rss/geral/feed.xml", # Agência Brasil - Geral
+    "http://agenciabrasil.ebc.com.br/rss/ultimasnoticias/feed.xml", # Agência Brasil - Últimas Notícias
+    "http://agenciabrasil.ebc.com.br/rss/politica/feed.xml", # Agência Brasil - Política
+    "http://agenciabrasil.ebc.com.br/rss/educacao/feed.xml", # Agência Brasil - Educação
+    "http://agenciabrasil.ebc.com.br/rss/saude/feed.xml", # Agência Brasil - Saúde
+]
+
 
 pt_br = " Responda em português brasileiro."
 
 # Used in process_articles (operates globally, so uses default)
 PROMPT_ARTICLE_SUMMARY = (
+    "Resuma os pontos principais desta notícia destacando os aspectos que podem indicar a presença de informação falsa ou enganosa, como recorte tendencioso, ausência de contraponto, reprodução de falas desinformativas sem apresentação de especialistas ou vozes dissonantes, uso de adjetivações fortes sem indicação de que se trata de opinião, ou linguagem chula e ataques diretos em artigos de opinião."
+    "Elabore esse resumo em 2 a 4 frases"
     "Identifique os principais tópicos abordados.\n\nArtigo:\n{article_content}." + pt_br
 )
 
 # Used in rate_articles (operates globally, so uses default)
-PROMPT_IMPACT_RATING = """Analise o artigo e os tópicos identificados da notícia a seguir e verifique se ela contém informações falsas ou enganosas.
+PROMPT_IMPACT_RATING = """Analise o resumo da notícia a seguir e verifique se ela contém informações falsas ou enganosas.
 Considere fatores como recorte tendencioso e sem apresentação de qualquer forma de contraponto. 
 Reprodução pura e simples de falas desinformativas de representantes do governo ou alguma autoridade sem a apresentação de um especialista ou uma voz dissonante em assuntos notadamente polêmicos. 
 Ou ainda que apresentasse a fala de representantes do governo ou autoridades de uma forma tendenciosa, emprestando adjetivos que fizessem possível inferir a aprovação ou desaprovação de tal fala pelo autor do texto, 
@@ -123,11 +157,32 @@ Alto risco de impacto social negativo.
 
 Exemplo: “Vacinas aplicadas em escolas causam infertilidade em estudantes.”
 
-Artigo:
-"{article_content}"
-Topicos Identificados:
+Resumo:
 "{summary}"
 
 Digite SOMENTE o número inteiro que representa sua classificação (1 a 10).
 """
 
+PROMPT_CLUSTER_ANALYSIS = (
+    """
+Estes são artigos de notícia potencialmente relacionados de um contexto '{feed_profile}':
+
+{cluster_summaries_text}
+
+QUal é o evento ou tópico principal discutido? Esse artigo contém informações falsas ou enganosas?
+Resuma os principais desenvolvimentos e indique se há desinformação expliando em 3 a 5 frases, com base *apenas* no texto fornecido. 
+Se os artigos parecerem não relacionados, informe isso claramente."""
++ pt_br
+)
+
+PROMPT_BRIEF_SYNTHESIS = """
+Você é um assistente de IA identificando notícias falsas e e descrevendo em markdown o motivo dessas notícias serem falsas e qual seria a verdade por trás do assunto tratado, 
+caso haja, especificamente para a categoria '{feed_profile}. 
+Elabora essas analises utilizando uma linguagem clara e acessível, evitando jargões técnicos, para que o público geral possa entender facilmente.
+
+EM seguida, aprese  nte fontes confiáveis que desmentem as notícias falsas, incluindo links para artigos de checagem de fatos, estudos científicos ou declarações oficiais que refutem as alegações enganosas.
+Se não houver evidências suficientes para classificar a notícia como falsa, explique por que ela é considerada verdadeira ou precisa, destacando as evidências que sustentam sua veracidade.
+
+Grupo de notícias analisadas:
+{cluster_analyses_text}
+"""
